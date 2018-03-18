f9dbd5f7ad4b62693f7532bd14b9a056d8c35fb4
Sunitha Ramakrishnan <ramakrishnansunitha950@gmail.com>
2018-03-17 23:08:13 -0700

    made some changes to the routes

--- static/js/plot.js
+++ static/js/plot.js
@@ -1,11 +1,7 @@
 var select = document.getElementById("samples");
 select.length = 0;
 
-<<<<<<< HEAD
 var names_url = "/names"
-=======
-var names_url = "/names"
->>>>>>> 89d991db68b36c4c656a6e08193f1d78765934af
 
 var defaultOption = document.createElement('option');
 defaultOption.text = 'Sample Names';
@@ -24,19 +20,10 @@ Plotly.d3.json(names_url, function(error, response){
         select.appendChild(el);
 }});
 
-<<<<<<< HEAD
-
-
 function optionChanged() {
     var selection = document.getElementById('samples');
     var selValue = selection.value;
     var sample_url = "/samples/" + selValue;
-=======
-function optionChanged() {
-    var selection = document.getElementById('samples');
-    var selValue = selection.value;
-    var sample_url = "/samples/" + selValue;
->>>>>>> 89d991db68b36c4c656a6e08193f1d78765934af
     Plotly.d3.json(sample_url, function(error, response){
         if (error) return console.warn(error);
         var otus = response[0]["otu_ids"];
@@ -54,19 +41,11 @@ function optionChanged() {
     })
 }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 89d991db68b36c4c656a6e08193f1d78765934af
  function findMetadata() {
     var selection = document.getElementById('samples');
     var selValue = selection.value;
     var metaList = document.getElementById('metadataList');
-<<<<<<< HEAD
     var meta_url = "/metadata/" + selValue;
-=======
-    var meta_url = "/metadata/" + selValue;
->>>>>>> 89d991db68b36c4c656a6e08193f1d78765934af
     Plotly.d3.json(meta_url, function(error, response){
     if (error) return console.warn.apply(error);
     var data = Object.entries(response[0]);
@@ -81,11 +60,7 @@ function optionChanged() {
 function createBubble() {
     var selection = document.getElementById('samples');
     var selValue = selection.value;
-<<<<<<< HEAD
     var sample_url = "/samples/" + selValue;
-=======
-    var sample_url = "/samples/" + selValue;
->>>>>>> 89d991db68b36c4c656a6e08193f1d78765934af
     Plotly.d3.json(sample_url, function(error, response){
         if (error) return console.warn(error);
         var otus = response[0]["otu_ids"];
@@ -118,8 +93,3 @@ function createBubble() {
         Plotly.newPlot('bubblePlot', data, layout);
     })
 };
-<<<<<<< HEAD
- 
-=======
- 
->>>>>>> 89d991db68b36c4c656a6e08193f1d78765934af
